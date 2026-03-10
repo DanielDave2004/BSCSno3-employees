c8167a14b39b6148d9790a5f564d9d89e5467171
Daniel Dave M. Umali <134363236+DanielDave2004@users.noreply.github.com>
2026-03-11 04:28:36 +0800

    Add files via upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,7 @@ exports.getAllUsers=(req,res)=>{
 //Search a user by ID
 exports.getUsersById=(req,res)=>{
     const id=req.params.id;
-    connection.query('SELECT * FROM userdata WHERE id=? OR email=?',[id,id],(err,rows,fields)=>{
+    connection.query('SELECT * FROM userdata WHERE id=?',[id],(err,rows,fields)=>{
         if(err) throw err;
         if(rows.length>0)
             res.json(rows);
@@ -24,41 +24,41 @@ exports.getUsersById=(req,res)=>{
 //Create a new user
 //CRUD - Create
 exports.createUser=(req,res)=>{
-    const {fname,lname,email,gender}=req.body;
-    connection.query('INSERT INTO userdata (first_name, last_name, email, gender) VALUES (?,?,?,?)',[fname,lname,email,gender],(err,result)=>{
+    const {fname,lname,email,gender} = req.body;
+    connection.query('INSERT INTO userdata (first_name, last_name, email, gender) VALUES (?,?,?,?)',
+      [fname,lname,email,gender],(err,result)=>{
         if(err) throw err;
-            res.json({message: 'User created succesfully', userId:result.insertId});
+            res.json({message: 'User Created Succesfully', userId:result.insertId});
     });
 };
 
-//Update a user
-//CRUD - Update
+// Update a user
+// CRUD - Update
 exports.updateUser = (req, res) => {
-  const { id,fname,lname,email,gender,course} = req.body;
-  connection.query(
-    "UPDATE userdata SET first_name=?, last_name=?, email=?, gender=?, course=? WHERE id=?",
-    [fname,lname,email,gender,course,id],
+  const {fname,lname,email,gender,id } = req.body;
+  connection.query("UPDATE userdata SET first_name=?, last_name=?, email=?, gender=? WHERE id=?",
+    [fname,lname,email,gender,id],
     (err, result) => {
       if (err) throw err;
       if (result.affectedRows > 0) {
-        res.json({ message: "User updated successfully" });
+        res.json({ message: "User Updated Successfully" });
       } else {
-        res.status(404).json({ message: "User not found" });
+        res.status(404).json({ message: "User Not Found" });
       }
-    },
+    }
   );
 };
 
 //Delete a user
 //CRUD - Delete
-exports.deleteUser = (req, res) => {
+exports.deleteUser = (req,res) => {
   const id = req.body.id;
-  connection.query("DELETE FROM userdata WHERE id=?", [id], (err, result) => {
+  connection.query("DELETE FROM userdata WHERE id=?",[id],(err, result) => {
     if (err) throw err;
     if (result.affectedRows > 0) {
-      res.json({ message: "User deleted successfully" });
+      res.json({ message: "User Deleted Successfully" });
     } else {
-      res.status(404).json({ message: "User not found" });
+      res.status(404).json({ message: "User Not Found" });
     }
   });
 };
